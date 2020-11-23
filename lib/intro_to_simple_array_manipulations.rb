3969de8f02316471b7af5d9de57aa0c5cddd2ba9
Alec Caro <alec.caro@gmail.com>
2020-11-23 02:04:24 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(arr, ele)
+  arr.push(ele)
+end
+
+def using_unshift(arr, ele)
+  arr.unshift(ele)
+end
+
+def using_pop(arr)
+  arr.pop
+end
+
+def pop_with_args(arr)
+  arr.pop(2)
+end
+
+def using_shift(arr)
+  arr.shift
+end
+
+def shift_with_args(arr)
+  arr.shift(2)
+end
